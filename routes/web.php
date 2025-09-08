80cd1fd5139359f9ffaf4f13010b3564549cd1d5
Jaswant Yadav <jaswantyadav742@gmail.com>
2025-09-08 17:45:20 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ Route::get('/', fn()=>redirect()->route('employees.index'));
 Route::resource('companies', CompanyController::class);
 Route::resource('employees', EmployeeController::class);
 
-Route::get('/employees-data', [EmployeeController::class, 'getData'])->name('employees.data');
+Route::get('/employees-data', [EmployeeController::class, 'Data'])->name('employees.data');
 
